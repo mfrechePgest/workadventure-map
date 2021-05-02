3e4cb991244a880ed58dde0f832a88600d211498
Mikaël FRECHE <mikael.freche@lacooperativewelcoop.com>
2021-05-02 16:30:03 +0200

    Ajout quelques logos

---
+++ codingame.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="codingame" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="codingame.png" width="96" height="96"/>
+</tileset>
---
+++ pharmagest192.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="pharmagest192" tilewidth="32" tileheight="32" tilecount="36" columns="6">
+ <image source="pharmagest192.png" width="192" height="192"/>
+</tileset>
